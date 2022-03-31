a610b18d22b504bae11455ad5297c4cfdde06a6f
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-03-31 17:45:03 +1300

    add rate limiting

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import express from 'express';
 import mongoose from 'mongoose';
 import router from './routes';
 import Config from './types/Config';
+import rateLimit from 'express-rate-limit';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { port, mongoURI }: Config = require('../config.json');
 
 const app = express();
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute (default)
+    max: 5, // default
+    standardHeaders: true,
+});
+
+app.use(limiter);
 
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
