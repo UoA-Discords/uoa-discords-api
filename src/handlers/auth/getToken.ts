ee64fca487f11405fbf2bd7bfeee8416278118a8
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-03 14:19:14 +1200

    wip

--- src/handlers/auth/getToken.ts
+++ src/handlers/auth/getToken.ts
@@ -1,14 +1,13 @@
+import { AccessTokenResponse } from '@uoa-discords/uoa-discords-shared-types';
 import axios, { AxiosError } from 'axios';
 import { Request, Response } from 'express';
-import { request } from 'https';
-import QueryString from 'qs';
 import Config from '../../types/Config';
-import { AccessTokenResponse, User } from '../../types/Discord';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { discordClientID: client_id, discordClientSecret: client_secret }: Config = require('../../../config.json');
 
-/** Gets a Discord access token.
+/**
+ * Upgrades a user's OAuth code into an access token.
  *
  * @see {@link https://discord.com/developers/docs/topics/oauth2#authorization-code-grant-refresh-token-exchange-example}
  */
@@ -32,34 +31,24 @@ async function getToken(req: Request, res: Response): Promise<void> {
         params.set('code', code);
         params.set('redirect_uri', redirect_uri);
 
-        const { data: OAuth } = await axios.post<AccessTokenResponse>(
-            'https://discord.com/api/v9/oauth2/token',
-            params,
-            {
+        try {
+            const { data } = await axios.post<AccessTokenResponse>('https://discord.com/api/v9/oauth2/token', params, {
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 },
-            },
-        );
+            });
+            res.status(200).json(data);
+        } catch (error) {
+            const axiosError = error as AxiosError;
+            if (axiosError?.isAxiosError) {
+                if (axiosError.response?.status === 400) {
+                    res.status(400).json(axiosError.response.data);
+                    return;
+                }
+            }
 
-        const {
-            data: { username, avatar, id, discriminator },
-        } = await axios.get<User>('https://discord.com/api/v9/users/@me', {
-            headers: {
-                Authorization: `Bearer ${OAuth.access_token}`,
-            },
-        });
-
-        res.status(200).json({
-            ...OAuth,
-            display: {
-                username,
-                avatar: `https://cdn.discordapp.com/avatars/${id}/${avatar}.png`,
-                id,
-                discriminator,
-            },
-        });
-        return;
+            throw error;
+        }
     } catch (error) {
         res.status(500).json(error instanceof Error ? error.message : 'Unknown error occurred');
     }
--- src/handlers/auth/refreshToken.ts
+++ src/handlers/auth/refreshToken.ts
@@ -1,12 +1,13 @@
-import axios from 'axios';
+import { AccessTokenResponse } from '@uoa-discords/uoa-discords-shared-types';
+import axios, { AxiosError } from 'axios';
 import { Request, Response } from 'express';
 import Config from '../../types/Config';
-import { AccessTokenResponse } from '../../types/Discord';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { discordClientID: client_id, discordClientSecret: client_secret }: Config = require('../../../config.json');
 
-/** Refreshes a Discord access token.
+/**
+ * Refreshes a Discord access token.
  *
  * @see {@link https://discord.com/developers/docs/topics/oauth2#authorization-code-grant-refresh-token-exchange-example}
  */
@@ -18,22 +19,31 @@ async function refreshToken(req: Request, res: Response): Promise<void> {
             return;
         }
 
-        const response = await axios.post<AccessTokenResponse>(
-            'https://discord.com/api/oauth2/token',
-            {
-                client_id,
-                client_secret,
-                grant_type: 'refresh_token',
-                refresh_token,
-            },
-            {
+        const params = new URLSearchParams();
+        params.set('client_id', client_id);
+        params.set('client_secret', client_secret);
+        params.set('grant_type', 'refresh_token');
+        params.set('refresh_token', refresh_token);
+
+        try {
+            const { data } = await axios.post<AccessTokenResponse>('https://discord.com/api/v9/oauth2/token', params, {
                 headers: {
                     'Content-Type': 'application/x-www-form-urlencoded',
                 },
-            },
-        );
+            });
+            res.status(200).json(data);
+        } catch (error) {
+            const axiosError = error as AxiosError;
+            if (axiosError?.isAxiosError) {
+                if (axiosError.response?.status === 400) {
+                    res.status(400).json(axiosError.response.data);
+                    return;
+                }
+            }
 
-        res.status(200).json(response);
+            throw error;
+        }
+
         return;
     } catch (error) {
         res.status(500).json(error instanceof Error ? error.message : 'Unknown error occurred');
