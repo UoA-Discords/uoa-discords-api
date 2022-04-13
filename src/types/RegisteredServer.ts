98d2332a8d91c618d304c4d8cfa4df3f431bd233
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-13 13:32:18 +1200

    fix unnecessary field declarations

--- src/types/RegisteredServer.ts
+++ src/types/RegisteredServer.ts
@@ -1,9 +1,6 @@
-import { Invite, PublicRegisteredGuild, User } from '@uoa-discords/shared-utils';
+import { PublicRegisteredGuild } from '@uoa-discords/shared-utils';
 
 export default interface RegisteredServer extends PublicRegisteredGuild {
     /** Guild ID used for indexing. */
     _id: string;
-    inviteObject: Invite;
-    addedBy: User;
-    approvedBy: User;
 }
