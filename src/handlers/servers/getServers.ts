e407057a4f1f055bc85209d2f326542890b5f572
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-18 23:16:14 +1200

    update get servers endpoint

--- src/handlers/servers/getServers.ts
+++ src/handlers/servers/getServers.ts
@@ -1,13 +1,39 @@
-import { GETRoutes } from '@uoa-discords/shared-utils';
+import { DiscordAPI, GETRoutes, Invite } from '@uoa-discords/shared-utils';
+import { ServerWithInviteInfo } from '@uoa-discords/shared-utils/dist/types/Types/ServerTypes';
 import { Request, Response } from 'express';
+import Caches from '../../classes/Caches';
 import ServerLogger from '../../classes/ServerLogger';
 import { RegisteredServerModel } from '../../models/RegisteredServerModel';
+
+async function getCacheOrAddTo(id: string, inviteCode: string): Promise<Invite | null> {
+    const existing = Caches.inviteCache.getItem(id);
+    if (existing) return existing;
+    const inviteQuery = await DiscordAPI.getInviteData(inviteCode);
+
+    if (inviteQuery.success) {
+        Caches.inviteCache.addItem(id, inviteQuery.data);
+        return inviteQuery.data;
+    }
+    ServerLogger.logError(GETRoutes.GetServers, inviteQuery.error);
+    return null;
+}
 
 async function getServers(req: Request, res: Response): Promise<void> {
     try {
         const servers = await RegisteredServerModel.find({});
 
-        res.status(200).json(servers);
+        const allServers = await Promise.all(servers.map((e) => getCacheOrAddTo(e._id, e.inviteCode)));
+
+        const validServers: ServerWithInviteInfo[] = [];
+
+        for (let i = 0, len = allServers.length; i < len; i++) {
+            const invite = allServers[i];
+            if (invite !== null) {
+                validServers.push({ ...servers[i], invite: invite });
+            }
+        }
+
+        res.status(200).json(validServers);
     } catch (error) {
         ServerLogger.logError(GETRoutes.GetServers, error);
         res.sendStatus(500);
