0a440539a9c05c52d98793518a9344b1904b3016
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-19 14:21:49 +1200

    add persistence to caches

--- src/classes/Caches.ts
+++ src/classes/Caches.ts
@@ -2,5 +2,5 @@ import { Invite } from '@uoa-discords/shared-utils';
 import CachedStorage from './template/CachedStored';
 
 export default abstract class Caches {
-    public static readonly inviteCache = new CachedStorage<Invite>();
+    public static readonly inviteCache = new CachedStorage<Invite>({ fileName: 'invites' });
 }
--- src/classes/template/CachedStored.ts
+++ src/classes/template/CachedStored.ts
@@ -1,3 +1,14 @@
+import DataManager from './DataManager';
+
+export interface CachedStorageProps {
+    /** Duration to store items, in seconds. Default is 5 minutes. */
+    duration?: number;
+
+    /** Filename of data to read/write to, include if you want
+     * the cache to persist between reloads. */
+    fileName?: string;
+}
+
 /** Cached item storer, stores a record/dictionary of values in memory. */
 export default class CachedStorage<T> {
     private _items: Record<string, { data: T; timeout: NodeJS.Timeout }> = {};
@@ -5,15 +16,21 @@ export default class CachedStorage<T> {
     /** Duration to store items, in seconds. */
     private _duration: number;
 
-    /** @param {number} duration - Duration to store items, in seconds. Default is 5 minutes. */
-    public constructor(duration: number = 5 * 60) {
+    private readonly _dataManager?: DataManager;
+
+    public constructor({ duration = 5 * 60, fileName }: CachedStorageProps) {
         this._duration = duration;
+        if (fileName) {
+            this._dataManager = new DataManager(`data/caches/${fileName}.json`, JSON.stringify({}, undefined, 4));
+            this._items = JSON.parse(this._dataManager.data);
+        }
     }
 
     /** Removes all items. */
     public clearAll(): void {
         Object.values(this._items).forEach(({ timeout }) => clearTimeout(timeout));
         this._items = {};
+        this.save();
     }
 
     public getItem(key: string): T | undefined {
@@ -24,26 +41,36 @@ export default class CachedStorage<T> {
 
     /** Adds an item to the cahce. */
     public addItem(key: string, value: T): void {
-        this.removeItem(key);
+        this.removeItem(key, true);
         this._items[key] = {
             data: value,
             timeout: setTimeout(() => {
                 this.removeItem(key);
             }, 1000 * this._duration),
         };
+
+        this.save();
     }
 
     /**
      * Removes an item from the cache.
      *
-     * @returns {boolean} Whether the item existed prior to deletion.
+     * @throws Throws an error if the specified key did not exist in the cache.
      */
-    public removeItem(key: string): boolean {
-        if (this._items[key]) {
-            clearTimeout(this._items[key].timeout);
-            delete this._items[key];
-            return true;
+    public removeItem(key: string, withoutSaving: boolean = false): void {
+        if (!this._items[key]) {
+            throw new Error(`Key ${key} does not exist in this cache`);
         }
-        return false;
+
+        clearTimeout(this._items[key].timeout);
+        delete this._items[key];
+
+        if (!withoutSaving) this.save();
+    }
+
+    public save(): void {
+        if (this._dataManager) {
+            this._dataManager.data = JSON.stringify(this._items, undefined, 4);
+        }
     }
 }
