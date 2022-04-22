732568df3589d9513094e9e882cfea395f917110
NachoToast <32235595+NachoToast@users.noreply.github.com>
2022-04-22 15:10:39 +1200

    add cache documentation

--- src/classes/Caches.ts
+++ src/classes/Caches.ts
@@ -2,7 +2,9 @@ import { Invite } from '@uoa-discords/shared-utils';
 import CachedStorage from './template/CachedStored';
 
 export default abstract class Caches {
+    /** Invite objects indexed by guild ID. */
     public static readonly inviteCache = new CachedStorage<Invite>({ fileName: 'invites' });
 
+    /** Array of guild IDs a user has liked, indexed by user ID. */
     public static readonly likesCache = new CachedStorage<string[]>({});
 }
